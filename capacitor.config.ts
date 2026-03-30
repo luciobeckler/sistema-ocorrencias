5d72f1177bc540af5955d8fdf749aa4b856e1d43
luciobeckler2015 <luciobeckler2015@gmail.com>
2026-03-30 11:41:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sistema-ocorrencias',
+  webDir: 'www'
+};
+
+export default config;
